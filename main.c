4a5f963c6314ceaa58a125b1556546200eb1af4d
Denis <ddd183@mail.ru>
2022-04-06 12:29:59 +0300

    no interruption.... blink led 9 via check CNT... stupid

--- main.c
+++ main.c
@@ -30,6 +30,12 @@ SOFTWARE.
 /* Includes */
 #include "stm32f0xx.h"
 
+void TIM6_DAC_IRQHandler(void) {
+	TIM6->SR &= ~TIM_SR_UIF;
+
+	blinkLed8();
+}
+
 void init_gpio() {
 	RCC->AHBENR |= RCC_AHBENR_GPIOCEN;	//turn on clock pulse
 	GPIOC->MODER |= GPIO_MODER_MODER8_0 | GPIO_MODER_MODER9_0;	//init gpio
@@ -39,16 +45,35 @@ void blinkLed8() {
 	GPIOC->ODR ^= GPIO_ODR_8;
 }
 
+void blinkLed9() {
+	if (TIM6->CNT > 4000) {
+		GPIOC->BSRR = GPIO_BSRR_BR_9;
+		return;
+	}
+
+	GPIOC->BSRR = GPIO_BSRR_BS_9;
+}
+
 void init_tim6() {
 	RCC->APB1ENR |= RCC_APB1ENR_TIM6EN;
+
+	TIM6->ARR = 8000 - 1;
+	TIM6->PSC = 500 - 1;
+
+	TIM6->DIER |= TIM_DIER_UIE;
+
+	NVIC_SetPriority(TIM6_DAC_IRQn, 8);
+	NVIC_EnableIRQ(TIM6_DAC_IRQn);
+
+	TIM6->CR1 |= TIM_CR1_CEN;
 }
 
 int main(void)
 {
 	init_gpio();
+	init_tim6();
 	while (1)
 	{
-		blinkLed8();
-		for(int i = 0; i < 100000; i++);
+		blinkLed9();
 	}
 }
